f45a94be93caa3d7bc95cbb8e20c050686329650
mhrndiva <ariadiva00@gmail.com>
2024-12-18 16:05:50 +0700

    pertemuan09

---
+++ pertemuan09/praktikum/praktikum_p09/README.md
@@ -0,0 +1,3 @@
+# praktikum_p09
+
+A new Flutter project.
---
+++ pertemuan09/praktikum/praktikum_p09/android/app/src/main/kotlin/com/example/praktikum_p09/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.praktikum_p09
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
